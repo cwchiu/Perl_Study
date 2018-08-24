9fbe78fa01469ed3b338d737c155bf51db31a7b5
Arick <sisimi.pchome@gmail.com>
2018-08-24 20:26:51 +0800

    file test

--- example/001_Syntax/file_test.pl
+++ example/001_Syntax/file_test.pl
@@ -1,29 +1,64 @@
+use 5.024;
 use strict;
 use warnings;
 use Test::More tests => 100;
+use Cwd qw(getcwd cwd);
 
-open FOUT, ">test.txt";
-print FOUT "#1\n";
-print FOUT "#2\n";
-close FOUT;
+subtest 'file read/write', sub {
+    my $filename = 'test.txt';
+    # perl 5.6+, 可分開為
+    # open FOUT, '>', filename;
+    open FOUT, ">${filename}";
+    print FOUT "#1\n";
+    print FOUT "#2\n";
+    close FOUT;
 
-open FAPPEND, '>>test.txt';
-print FAPPEND "#3\n";
-close FAPPEND;
+    ok((-e $filename), 'check file exists');
+    ok((-f $filename), 'check file');
+  
+  
+    open FAPPEND, ">>$filename";
+    print FAPPEND "#3\n";
+    close FAPPEND;
 
-open FIN, '<test.txt';
-while (<FIN>){
-  print $_    
-}
-close FIN;
+    open FIN, "<$filename";
+    while (<FIN>){
+      print $_    
+    }
+    close FIN;
+    
+    ok((-B 'c:\\windows\\notepad.exe'), 'binary file');
+    
+    open FIN, "<$filename";
+    my @lines = <FIN>;
+    ok(scalar @lines == 3);
+    close FIN;
+};
 
-ok((-e 'test.txt'), 'check file exists');
-ok((-f 'test.txt'), 'check file');
-ok((-B 'c:\\windows\\notepad.exe'), 'binary file');
+subtest 'handle file open warn', sub {
+    my $open_ok = open FFAIL, "not found file";
+    ok( !$open_ok);
+    
+    open FFAIL, 'not found file' or warn 'file not found';
+    
+    my $eval_ok = eval {
+        my $ret = 1;
+        open FFAIL, 'not found file' or $ret = 0;
+        
+        $ret;
+    };
+    ok( $eval_ok == 0, 'handle warn');   
+};
 
-open FFAIL, 'not found file' or warn 'file not found';
+subtest 'chdir', sub {
+    my $folder = getcwd();    
+    my $folder2 = cwd();
+    ok( $folder eq $folder2);
+    chdir "c:\\";
+    ok(cwd() eq "c:/");
+    chdir $folder;
+};
 
-# chdir "/tmp";
 # chmod 0444, 'log.txt';
 # chown 
 # link "log.txt", "log.bak";
@@ -35,12 +70,20 @@ open FFAIL, 'not found file' or warn 'file not found';
 # utime
 # localtime
 
-my @filelist = glob "*.pl";
-print $#filelist;
-for (@filelist) {
-  print ">$_<\n";    
+sub readfiles() {
+    my @filelist = glob "*.pl";
+    print $#filelist;
+    for (@filelist) {
+      print ">$_<\n";    
+    }
+
+    for (<*.pl>) {
+      print ">$_<\n";    
+    }
 }
 
-for (<*.pl>) {
-  print ">$_<\n";    
+sub main(){
+
 }
+    
+main();
