eba8d7f62aa034243b7c8fcf73d1dfbc36ad4bf8
Arick <sisimi.pchome@gmail.com>
2018-08-25 22:25:49 +0800

    date time test

--- example/001_Syntax/datetime_test.pl
+++ example/001_Syntax/datetime_test.pl
@@ -1,5 +1,42 @@
+use 5.024; # 標記使用 perl 5.24
 
-my $t = time;
-print $t;
-print '>' . ref($t) . '<';
-# print reftype($t);
+# 避免常見錯誤
+use strict;
+use warnings;
+use Test::More tests => 100;
+use POSIX qw(strftime);
+
+sub now(){
+    my $t = time;
+    print "Now Timestamp: $t\n";
+
+    my $d = localtime;
+    print("Now String: $d\n");
+
+    my @d = localtime;
+    my ($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst) = @d;
+    for (@d) {
+        print "$_\n";
+    }
+}
+
+sub main(){
+    now();
+    
+    
+}
+
+subtest 'localtime', sub {
+    my $t = 1535206078;
+    my @dt = localtime($t);
+    my ($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst) = @dt;
+    ok( $mon == 7);
+    ok( $mday == 25);
+    ok( $year+1900 == 2018);
+    ok( $hour == 22);
+    ok( $min == 7);
+    ok( $sec == 58);
+    
+};
+
+main();
